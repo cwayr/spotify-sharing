73e0527a115ae4f5186e0c87e9f148b5ec0d0e20
Caleb Waymeyer <cjwaymeyer@gmail.com>
2021-12-04 13:49:32 -0500

    create new-group button

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
+from wtforms import StringField, PasswordField, TextAreaField
 from wtforms.validators import DataRequired, Length, EqualTo
 
 
@@ -15,3 +15,9 @@ class RegisterForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()], description="username")
     password = PasswordField('Password', validators=[DataRequired(), Length(min=6, message="Password must be at least 6 characters.")], description="password")
     confirm_password = PasswordField('Confirm password', validators=[DataRequired(), EqualTo('password', message="Passwords must match.")], description="confirm password")
+
+
+class NewGroupForm(FlaskForm):
+
+    name = StringField("Title", validators=[DataRequired()], description="name")
+    description = TextAreaField("Description", description="description")
