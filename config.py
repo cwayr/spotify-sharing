6e3a53dd830d19425da4e7af969495bdee6f0456
Caleb Waymeyer <cjwaymeyer@gmail.com>
2022-01-21 22:22:00 -0500

    db

--- config.py
+++ config.py
@@ -36,6 +36,7 @@ class ProductionConfig(Config):
     """Configurations for production."""
 
     DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
     ENV = 'production'
 
 
